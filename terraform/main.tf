b8db0e477fe4c1d1628458d90542111dae3756db
Mayank Siwach <maxsiwach@gmail.com>
2025-09-22 09:39:20 +0000

    Your commit message for the empty commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,24 +5,28 @@ resource "aws_instance" "strapi" {
   vpc_security_group_ids = [aws_security_group.strapi.id]
 
   user_data = <<-EOF
-              #!/bin/bash
-              dnf update -y
-              dnf install -y docker unzip
-              systemctl enable docker
-              systemctl start docker
-              usermod -aG docker ec2-user
+            #!/bin/bash
+            # Update system
+            dnf update -y
 
-              # Install AWS CLI
-              curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-              unzip awscliv2.zip
-              ./aws/install
+            # Install Docker
+            dnf install -y docker unzip
+            systemctl enable docker
+            systemctl start docker
+            usermod -aG docker ec2-user
 
-              # Login to ECR
-              aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin 145065858967.dkr.ecr.${var.aws_region}.amazonaws.com
+            # Install AWS CLI v2
+            curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+            unzip awscliv2.zip
+            ./aws/install
 
-              # Run Strapi container
-              docker run -d -p 80:1337 ${var.docker_image}
-              EOF
+            # Login to ECR
+            aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin 145065858967.dkr.ecr.${var.aws_region}.amazonaws.com
+
+            # Pull and run Strapi container
+            docker pull ${var.docker_image}
+            docker run -d -p 80:1337 ${var.docker_image}
+            EOF
 
   tags = {
     Name = "Strapi-Instance"
